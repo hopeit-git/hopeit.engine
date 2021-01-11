c4ccad490aa46f9a248024656edb82d1151e1291
Pablo R. Canto <pcanto@hopeit.com.ar>
2021-01-11 16:06:21 -0300

    update version revision

--- engine/setup.py
+++ engine/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="hopeit.engine",
-    version="0.1.1",
+    version="0.1.2",
     description="Hopeit.py Engine",
     package_dir={
         "": "src"
