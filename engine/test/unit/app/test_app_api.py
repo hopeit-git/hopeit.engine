3b4ff70312b9676e56a3ba6d80a3057fba825f76
Pablo R. Canto <pcanto@hopeit.com.ar>
2021-02-10 20:03:39 -0300

    add test for event_api title parameter

--- engine/test/unit/app/test_app_api.py
+++ engine/test/unit/app/test_app_api.py
@@ -5,7 +5,7 @@ import pytest
 import hopeit.app.api as api
 from hopeit.app.config import EventType
 from hopeit.server.api import APIError
-from mock_app import mock_app_api_get, MockData, mock_app_api_post, mock_app_api_get_list
+from mock_app import mock_app_api_get, MockData, mock_app_api_post, mock_app_api_get_list, mock_app_api_title
 from mock_app import mock_api_app_config, mock_api_spec  # noqa: F401
 
 
@@ -55,6 +55,31 @@ def test_event_api_post(monkeypatch, mock_api_spec, mock_api_app_config):  # noq
         query_args=['arg1'],
         responses={200: int}
     )(mock_app_api_post, app_config=mock_api_app_config, event_name='mock-app-api-post', plugin=None)
+    assert spec['summary'] == \
+        mock_api_spec['paths']['/api/mock-app-api/test/mock-app-api']['post']['summary']
+    assert spec['description'] == \
+        mock_api_spec['paths']['/api/mock-app-api/test/mock-app-api']['post']['description']
+    assert spec['parameters'][0] == \
+        mock_api_spec['paths']['/api/mock-app-api/test/mock-app-api']['post']['parameters'][0]
+    assert spec['requestBody'] == \
+        mock_api_spec['paths']['/api/mock-app-api/test/mock-app-api']['post']['requestBody']
+    assert spec['responses'] == \
+        mock_api_spec['paths']['/api/mock-app-api/test/mock-app-api']['post']['responses']
+
+
+def test_event_api_title(monkeypatch, mock_api_spec, mock_api_app_config):  # noqa: F811
+    monkeypatch.setattr(api, 'spec', mock_api_spec)
+    mock_api_spec['paths']['/api/mock-app-api/test/mock-app-api']['post']['parameters'][0]['description'] = \
+        'arg1'
+    mock_api_spec['paths']['/api/mock-app-api/test/mock-app-api']['post']['requestBody']['description'] = \
+        'MockData'
+    spec = api.event_api(
+        title="Test app api part 2",
+        description="Description Test app api part 2",
+        payload=MockData,
+        query_args=['arg1'],
+        responses={200: int}
+    )(mock_app_api_title, app_config=mock_api_app_config, event_name='mock-app-api-post', plugin=None)
     assert spec['summary'] == \
         mock_api_spec['paths']['/api/mock-app-api/test/mock-app-api']['post']['summary']
     assert spec['description'] == \
