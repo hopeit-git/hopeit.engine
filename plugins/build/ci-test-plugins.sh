9a3c5cab8c579072ea1b361d9e88c11974d866b2
Leo <leosmerling-hopeit@noreply.github.com>
2021-05-09 00:31:12 +0200

    Main PyPi publishing

--- plugins/build/ci-test-plugins.sh
+++ plugins/build/ci-test-plugins.sh
@@ -3,11 +3,11 @@ echo "CI TEST: PLUGINS"
 echo "================"
 code=0
 # auth/basic-auth
-export PYTHONPATH=engine/src/:plugins/auth/basic-auth/src/ && python3 -m pytest --cov-fail-under=90 --cov-report=term --cov=plugins/auth/basic-auth/src/ plugins/auth/basic-auth/test/unit/ plugins/auth/basic-auth/test/integration/
+export PYTHONPATH=engine/src/:plugins/auth/basic-auth/src/ && python3 -m pytest -v --cov-fail-under=90 --cov-report=term --cov=plugins/auth/basic-auth/src/ plugins/auth/basic-auth/test/unit/ plugins/auth/basic-auth/test/integration/
 code+=$?
 
 # streams/redis
-export PYTHONPATH=engine/src/:plugins/streams/redis/src/ && python3 -m pytest --cov-fail-under=90 --cov-report=term --cov=plugins/streams/redis/src/ plugins/streams/redis/test/unit/
+export PYTHONPATH=engine/src/:plugins/streams/redis/src/ && python3 -m pytest -v --cov-fail-under=90 --cov-report=term --cov=plugins/streams/redis/src/ plugins/streams/redis/test/unit/
 code+=$?
 
 if [ $code -gt 0 ]
