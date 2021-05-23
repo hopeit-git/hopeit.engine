56ac7b8ab6959ada5617589e706d7f6869a75128
Leo <leosmerling@noreply.github.com>
2021-05-23 14:20:45 +0200

    Improved labels

--- plugins/ops/apps-visualizer/src/hopeit/apps_visualizer/events_graph.py
+++ plugins/ops/apps-visualizer/src/hopeit/apps_visualizer/events_graph.py
@@ -12,7 +12,7 @@ from hopeit.apps_visualizer.graphs import Graph, get_edges, get_nodes
 from hopeit.server.imports import find_event_handler
 from hopeit.server.steps import split_event_stages
 
-__steps__ = ['generate_config_graph']
+__steps__ = ['generate_config_graph', 'build_cytoscape_data']
 
 _dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
 
@@ -33,13 +33,13 @@ async def generate_config_graph(payload: None, context: EventContext) -> Graph:
     return Graph(nodes=nodes, edges=edges)
 
 
-def _build_cytoscape_data(graph: Graph) -> str:
+def build_cytoscape_data(graph: Graph, context: EventContext) -> str:
     # node_index = {node.id: node for node in graph.nodes}
     nodes = [
         {"data": {
             "group": node.type.value,
             "id": node.id,
-            "content": node.label
+            "content": '\n'.join(node.label.split('.'))
         }}
         for node in graph.nodes
     ]
@@ -48,7 +48,8 @@ def _build_cytoscape_data(graph: Graph) -> str:
             "group": "edges",
             "id": f"edge_{edge.id}",
             "source": edge.source,
-            "target": edge.target
+            "target": edge.target,
+            "label": edge.label.split('.')[-1]
         }}
         for edge in graph.edges
     ]
@@ -56,10 +57,9 @@ def _build_cytoscape_data(graph: Graph) -> str:
     return json.dumps(data)
 
 
-async def __postprocess__(graph: Graph, context: EventContext, response: PostprocessHook) -> str:
+async def __postprocess__(data: str, context: EventContext, response: PostprocessHook) -> str:
     # TODO: Read template, build graph data and return text/html response
     with open(_dir_path / 'events_graph_template.html') as f:
         template = f.read()
-        data = _build_cytoscape_data(graph)
         response.set_content_type("text/html")
         return template.replace("{data}", data)
