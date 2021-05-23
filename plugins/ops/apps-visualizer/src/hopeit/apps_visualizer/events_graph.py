5043f74a4ce6739d9bf1d9ce6cba780ead5dbb62
Leo <leosmerling@noreply.github.com>
2021-05-23 14:06:42 +0200

    Render cytospace template

--- plugins/ops/apps-visualizer/src/hopeit/apps_visualizer/events_graph.py
+++ plugins/ops/apps-visualizer/src/hopeit/apps_visualizer/events_graph.py
@@ -1,7 +1,10 @@
 """
 Events graph showing events, stream and dependecies for specified apps
 """
+import os
 import sys
+import json
+from pathlib import Path
 
 from hopeit.app.context import EventContext, PostprocessHook
 
@@ -10,6 +13,8 @@ from hopeit.server.imports import find_event_handler
 from hopeit.server.steps import split_event_stages
 
 __steps__ = ['generate_config_graph']
+
+_dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
 
 
 async def generate_config_graph(payload: None, context: EventContext) -> Graph:
@@ -28,14 +33,33 @@ async def generate_config_graph(payload: None, context: EventContext) -> Graph:
     return Graph(nodes=nodes, edges=edges)
 
 
+def _build_cytoscape_data(graph: Graph) -> str:
+    # node_index = {node.id: node for node in graph.nodes}
+    nodes = [
+        {"data": {
+            "group": node.type.value,
+            "id": node.id,
+            "content": node.label
+        }}
+        for node in graph.nodes
+    ]
+    edges = [
+        {"data": {
+            "group": "edges",
+            "id": f"edge_{edge.id}",
+            "source": edge.source,
+            "target": edge.target
+        }}
+        for edge in graph.edges
+    ]
+    data = [*nodes, *edges]
+    return json.dumps(data)
+
+
 async def __postprocess__(graph: Graph, context: EventContext, response: PostprocessHook) -> str:
     # TODO: Read template, build graph data and return text/html response
-    response.set_content_type("text/html")
-    return """
-    <html>
-    <h1> Apps! </h1>
-    <body>
-    {}
-    </body>
-    <html>
-    """.format(graph.to_json())
+    with open(_dir_path / 'events_graph_template.html') as f:
+        template = f.read()
+        data = _build_cytoscape_data(graph)
+        response.set_content_type("text/html")
+        return template.replace("{data}", data)
