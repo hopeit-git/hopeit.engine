9542c3b53c4c01fe1e1c7d6cf51a3c1dd19d260a
Leo <leosmerling-hopeit@noreply.github.com>
2021-05-25 01:36:06 +0200

    Apps selector

--- plugins/ops/apps-visualizer/src/hopeit/apps_visualizer/events_graph.py
+++ plugins/ops/apps-visualizer/src/hopeit/apps_visualizer/events_graph.py
@@ -3,7 +3,7 @@ Events graph showing events, stream and dependecies for specified apps
 """
 import os
 import sys
-from typing import Optional
+from typing import List, Optional
 import json
 from pathlib import Path
 
@@ -16,10 +16,12 @@ from hopeit.server.steps import split_event_stages
 from hopeit.app.api import event_api
 from hopeit.dataobjects import dataclass, dataobject
 from hopeit.app.events import collector_step, Collector
+from hopeit.app.config import AppConfig
 
 __steps__ = [
     'visualization_options',
     collector_step(payload=VisualizationOptions).gather(
+        'runtime_apps',
         'config_graph',
         'cytoscape_data'
     ),
@@ -43,7 +45,14 @@ _dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
 
 @dataobject
 @dataclass
+class RuntimeApps:
+    apps: List[AppConfig]
+
+
+@dataobject
+@dataclass
 class EventsGraphResult:
+    runtime_apps: RuntimeApps
     graph: CytoscapeGraph
     options: VisualizationOptions
 
@@ -57,24 +66,39 @@ async def visualization_options(payload: None, context: EventContext,
     )
 
 
+async def runtime_apps(collector: Collector, context: EventContext) -> RuntimeApps:
+    """
+    Extract current runtime app_config objects
+    """
+    server = getattr(sys.modules.get("hopeit.server.runtime"), "server")
+    return RuntimeApps(
+        apps=sorted(
+            (app.app_config for app in server.app_engines.values()),
+            key=lambda x: x.app_key()
+        )
+    )
+
+
 async def config_graph(collector: Collector, context: EventContext) -> Graph:
     """
     Generates Graph object with nodes and edges from server runtime active configuration
     """
     options: VisualizationOptions = await collector['payload']
-    app_prefix = options.app_prefix.replace('-', '_')
-    server = getattr(sys.modules.get("hopeit.server.runtime"), "server")
-    events = {}
-    for app_key, app in server.app_engines.items():
-        if app_prefix and app_key[0:len(app_prefix)] != app_prefix:
-            continue
+    all_apps: RuntimeApps = await collector['runtime_apps']
+    filterd_apps = (
+        app_config for app_config in all_apps.apps
+        if options.app_prefix == '' or (
+            app_config.app.name[0:len(options.app_prefix)] == options.app_prefix
+        )
+    )
 
-        app_config = app.app_config
+    events = {}
+    for app_config in filterd_apps:
         for event_name, event_info in app_config.events.items():
             impl = find_event_handler(app_config=app_config, event_name=event_name)
             splits = split_event_stages(app_config.app, event_name, event_info, impl)
             for name, info in splits.items():
-                events[f"{app_key}.{name}"] = info
+                events[f"{app_config.app_key()}.{name}"] = info
 
     nodes = get_nodes(events, expand_queues=options.expand_queues)
     edges = get_edges(nodes)
@@ -120,6 +144,7 @@ async def cytoscape_data(collector: Collector, context: EventContext) -> Cytosca
 
 async def build_visualization(collector: Collector, context: EventContext) -> EventsGraphResult:
     return EventsGraphResult(
+        runtime_apps=await collector['runtime_apps'],
         graph=await collector['cytoscape_data'],
         options=await collector['payload']
     )
@@ -131,6 +156,22 @@ async def __postprocess__(result: EventsGraphResult, context: EventContext, resp
     """
     response.set_content_type("text/html")
 
+    app_prefix = result.options.app_prefix
+    app_names = [
+        ("All runtime apps", ""),
+        *((app_config.app.name, app_config.app.name) for app_config in result.runtime_apps.apps)
+    ]
+    app_items = '\n'.join(
+        [
+            f'<li><a class="dropdown-item'
+            f'{" active" if name[0:len(app_prefix)] == app_prefix else ""}"'
+            f' href="events-graph?app_prefix={prefix}'
+            f"&expand_queues={str(result.options.expand_queues).lower()}"
+            f'">{name}</a></li>'
+            for name, prefix in app_names
+        ]
+    )
+
     switch_link = f"events-graph?app_prefix={result.options.app_prefix}"
     switch_link += f"&expand_queues={str(not result.options.expand_queues).lower()}"
 
@@ -139,6 +180,7 @@ async def __postprocess__(result: EventsGraphResult, context: EventContext, resp
 
     with open(_dir_path / 'events_graph_template.html') as f:
         template = f.read()
+        template = template.replace("{{ app_items }}", app_items)
         template = template.replace("{{ app_prefix }}", app_prefix)
         template = template.replace("{{ switch_link }}", switch_link)
         template = template.replace("{{ view_type }}", view_type)
