5ebac6721915555310782f04a687ef31923aa734
Leo <leosmerling@noreply.github.com>
2022-03-27 20:57:17 +0200

    fs stream batch storage it tests

---
+++ plugins/storage/fs/test/integration/conftest.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from datetime import datetime, timezone
+import uuid
+import pytest  # type: ignore
+from hopeit.testing.apps import config
+
+from hopeit.dataobjects import dataobject
+
+from hopeit.fs_storage import FileStorageSettings
+
+
+@dataobject(event_id="object_id", event_ts="object_ts")
+@dataclass
+class TestObject:
+    object_id: str
+    object_ts: datetime
+    value: int
+
+
+@pytest.fixture
+def test_objs():
+    # Creates 10 elements, in 5 partitions
+    return [
+        TestObject(
+            object_id=f"obj{i}",
+            object_ts=datetime.strptime(f"2020-05-01T0{i//2}:00:00+00:00", "%Y-%m-%dT%H:%M:%S%z").astimezone(tz=timezone.utc),
+            value=i
+        ) for i in range(10)
+    ]
+
+
+@pytest.fixture
+def app_config():
+    cfg = config("plugins/storage/fs/test/integration/app_config.json")
+    # Set unique folder for each test run
+    cfg.settings["test_stream_batch_storage"]["path"] += f"/{str(uuid.uuid4())}"
+    return cfg
