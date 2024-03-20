8f661a89d160071d1f9df7dfe9d1ea1d83a0fac1
Leo Smerling <61629371+leosmerling-hopeit@users.noreply.github.com>
2024-03-20 10:43:33 -0300

    Fixed dataframes example app api

--- apps/examples/dataframes-example/api/create_openapi_file.sh
+++ apps/examples/dataframes-example/api/create_openapi_file.sh
@@ -4,5 +4,5 @@ hopeit_openapi create \
 --title="Dataframes Example" \
 --description="Dataframes Example" \
 --api-version="$(python -m hopeit.server.version APPS_API_VERSION)" \
---config-files=engine/config/dev-local.json,plugins/ops/config-manager/config/plugin-config.json,plugins/data/dataframes/config/plugin-config.json,apps/examples/dataframes-example/config/app-config.json \
+--config-files=engine/config/dev-local.json,plugins/auth/basic-auth/config/plugin-config.json,plugins/ops/config-manager/config/plugin-config.json,plugins/data/dataframes/config/plugin-config.json,apps/examples/dataframes-example/config/app-config.json \
 --output-file=apps/examples/dataframes-example/api/openapi.json
