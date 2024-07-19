20a868b51a638c17d47401c0d8e55a500f06f718
Leo Smerling <leosmerling@hopeit.com.ar>
2024-07-19 19:29:44 +0200

    make it fail

--- engine/src/hopeit/dataobjects/payload.py
+++ engine/src/hopeit/dataobjects/payload.py
@@ -2,7 +2,7 @@
 Payload tools to serialize and deserialze event payloads and responses, including dataobjects
 """
 
-from typing import Type, Generic, Union, Dict
+from typing import Type, Generic, Union, Dict, Any
 
 from pydantic import RootModel, ValidationError
 
