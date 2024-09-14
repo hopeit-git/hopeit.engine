8602ab2d48a11d30034f77b7a0e756270faa9af5
Pablo Canto <pcanto@hopeit.com.ar>
2024-09-14 09:41:29 -0300

    requirements from toml

--- engine/setup.py
+++ engine/setup.py
@@ -1,44 +1,28 @@
 from setuptools import setup
-import re
-
-DEPS = "requirements.txt"
 
 version = {}
 with open("src/hopeit/server/version.py") as fp:
     exec(fp.read(), version)
     ENGINE_VERSION = version["ENGINE_VERSION"]
 
-
-def read_requirements_txt(*packages):
-    """Return lines from the file that match the exact package names with optional version specifiers."""
-    matches = []
-    package_patterns = [
-        re.compile(rf"^{re.escape(package)}(\s|==|>=|<=|>|<|;|$)", re.IGNORECASE)
-        for package in packages
-    ]
-
-    with open(DEPS, "r") as file:
-        for line in file:
-            line = line.strip()
-            if line and not line.startswith("#"):
-                if any(pattern.match(line) for pattern in package_patterns):
-                    matches.append(line)
-    return matches
-
-
 setup(
-    install_requires=read_requirements_txt(
-        "lz4",
-        "stringcase",
-        "PyJWT[crypto]",
+    install_requires=[
+        "pydantic>=2.9.1,<3",
+        "stringcase>=1.2.0",
+        "lz4>=4.3.2",
+        "PyJWT[crypto]>=2.9.0",
         "deepdiff",
         "typing-inspect",
         "multidict",
-        "pydantic",
-    ),
+    ],
     extras_require={
-        "web": read_requirements_txt("aiohttp", "aiohttp-cors", "aiohttp-swagger3", "gunicorn"),
-        "cli": read_requirements_txt("click"),
+        "web": [
+            "aiohttp>=3.9.0,<4",
+            "aiohttp-cors",
+            "aiohttp-swagger3>=0.8.0",
+            "gunicorn",
+        ],
+        "cli": ["click"],
         "redis-streams": [f"hopeit.redis-streams=={ENGINE_VERSION}"],
         "redis-storage": [f"hopeit.redis-storage=={ENGINE_VERSION}"],
         "fs-storage": [f"hopeit.fs-storage=={ENGINE_VERSION}"],
