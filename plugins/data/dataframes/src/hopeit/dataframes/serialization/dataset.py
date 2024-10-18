54d4fe5f7683950fa4c5e1186a823c8593bfdcf1
leosmerling-hopeit <61629371+leosmerling-hopeit@users.noreply.github.com>
2024-10-18 10:02:22 +0200

    Support to load/save raw pandas dataframes

--- plugins/data/dataframes/src/hopeit/dataframes/serialization/dataset.py
+++ plugins/data/dataframes/src/hopeit/dataframes/serialization/dataset.py
@@ -4,8 +4,11 @@ from importlib import import_module
 from typing import Any, Dict, Generic, Type, TypeVar
 
 from hopeit.dataobjects import dataclass, dataobject, field
+import pandas as pd
+from pydantic import TypeAdapter
 
 DataFrameT = TypeVar("DataFrameT")
+GenericDataFrameT = TypeVar("GenericDataFrameT")
 
 
 class DatasetLoadError(Exception):
@@ -24,18 +27,42 @@ class Dataset(Generic[DataFrameT]):
     schema: Dict[str, Any] = field(default_factory=dict)
 
     async def load(self) -> DataFrameT:
+        df = await self.load_df()
+        return self.convert(self, df)
+
+    async def load_df(self, *, columns: list[str] | None = None) -> pd.DataFrame:
         try:
-            dataframe = await self.__storage.load(self)  # type: ignore[attr-defined]
-            return dataframe
-        except (RuntimeError, IOError, KeyError) as e:
+            return await self.__storage.load_df(self, columns)  # type: ignore[attr-defined]
+        except (RuntimeError, IOError) as e:
             raise DatasetLoadError(
                 f"Error {type(e).__name__}: {e} loading dataset of type {self.datatype} "
                 f"at location {self.partition_key}/{self.key}"
             ) from e
 
+    def convert(self, dataset: "Dataset", df: pd.DataFrame) -> DataFrameT:
+        """Converts loaded pandas Dataframe to @dataframe annotated object using Dataset metadata"""
+        datatype: Type[DataFrameT] = find_dataframe_type(dataset.datatype)
+        return datatype._from_df(df)  # type: ignore[attr-defined]
+
+    def adapt(self, datatype: DataFrameT) -> "Dataset[DataFrameT]":
+        """Adapts a more generic dataset that contains combined fields to be type specific"""
+        return Dataset(
+            protocol=self.protocol,
+            partition_key=self.partition_key,
+            key=self.key,
+            datatype=f"{datatype.__module__}.{datatype.__qualname__}",  # type: ignore[attr-defined]
+            schema=TypeAdapter(datatype).json_schema(),
+        )
+
     @classmethod
     async def save(cls, dataframe: DataFrameT) -> "Dataset[DataFrameT]":
         return await cls.__storage.save(dataframe)  # type: ignore[attr-defined]
+
+    @classmethod
+    async def save_df(
+        cls, df: pd.DataFrame, datatype: Type[GenericDataFrameT]
+    ) -> "Dataset[GenericDataFrameT]":
+        return await cls.__storage.save_df(df, datatype)  # type: ignore[attr-defined]
 
 
 def find_protocol_impl(qual_type_name: str) -> Type:
@@ -45,4 +72,18 @@ def find_protocol_impl(qual_type_name: str) -> Type:
     )
     module = import_module(mod_name)
     datatype = getattr(module, type_name)
+    return datatype
+
+
+def find_dataframe_type(qual_type_name: str) -> Type[DataFrameT]:
+    """Returns dataframe class based on type name used during serialization"""
+    mod_name, type_name = (
+        ".".join(qual_type_name.split(".")[:-1]),
+        qual_type_name.split(".")[-1],
+    )
+    module = import_module(mod_name)
+    datatype = getattr(module, type_name)
+    assert hasattr(
+        datatype, "__dataframe__"
+    ), f"Type {qual_type_name} must be annotated with `@dataframe`."
     return datatype
